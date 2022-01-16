ffbcdf23ce2c34b713936c230c69fc89397efe9a
reiko_iguchi <reiko6122@gmail.com>
2022-01-16 11:03:26 +0900

    first-commit

---
+++ resources/views/thanks.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>お問い合わせ完了</title>
+</head>
+<body>
+  <p>お問い合わせありがとうございます。</p>
+  <a href="{{ url('/') }}">トップへ戻る</a>
+</body>
+</html>
